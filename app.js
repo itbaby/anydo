826d072c7f9fb1d92d16dcc394d8ad96d73f8f3a
wei.li <wei.li@TABLET-IOCQ7P5H.local>
2017-03-20 21:14:54 +0800

    done

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const Koa = require('koa');
 const res = require('koa-static');
+const body = require('koa-better-body');
 
 const app = new Koa();
+
+app.use(body());
 app.use(res('htm', { gzip: true }));
+
+
+
 
 require('./ctrl/welcome')(app);
 
-app.listen(3000, () => {
-   
-})
+app.listen(3000)
--- ctrl/welcome.js
+++ ctrl/welcome.js
@@ -1,11 +1,12 @@
 const Router = require('koa-router');
 module.exports = (app) => {
-  const router = new Router({
-          prefix: '/welcome'
-      });
-  router.get('/', async (ctx, next) => {
-        ctx.body = {status:'done'};
-    });    
-   app.use(router.routes())
+    const router = new Router({
+        prefix: '/welcome'
+    });
+    router.get('/', async(ctx, next) => {
+        ctx.body = 'Hello Users';
+    });
+
+    app.use(router.routes())
         .use(router.allowedMethods());
 }
