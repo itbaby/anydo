4f2e81a6adb3c007929040c38c0b86949c8787e2
wei.li <wei.li@TABLET-IOCQ7P5H.local>
2017-03-20 19:36:21 +0800

    set up service

--- ctrl/welcome.js
+++ ctrl/welcome.js
@@ -1,9 +1,11 @@
 const Router = require('koa-router');
 module.exports = (app) => {
   const router = new Router({
-          prefix: '/login'
+          prefix: '/welcome'
       });
-      
+  router.get('/', async (ctx, next) => {
+        ctx.body = {status:'done'};
+    });    
    app.use(router.routes())
         .use(router.allowedMethods());
 }
